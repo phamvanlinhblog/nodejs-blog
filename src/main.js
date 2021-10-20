73695e97879225a479eb13853a16ec4b4b5d5fcb
Phạm Văn Linh <85024995+kulihhihi@users.noreply.github.com>
2021-10-20 11:50:34 +0700

    i prettier lint-staged husky

--- src/main.js
+++ src/main.js
@@ -16,12 +16,12 @@ app.use(morgan('combined'));
 
 // Template engine
 app.engine(
-     'hbs',
-          handlebars({
-                extname: '.hbs',
-            }),
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
 );
-        app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
